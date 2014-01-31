527b5e12cbf34f1bd8575c344d26406e2936eb5a
brwnj <brwnjm@gmail.com>
2014-01-31 14:24:40 -0700

    package as console script

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+_locals = {}
+with open('umitools/_version.py') as fp:
+    exec(fp.read(), None, _locals)
+version = _locals['__version__']
+
+setup(name='umitools',
+    version=version,
+    description='Handle reads with an incorporated UMI',
+    author='Joe Brown, Jay Hesselberth',
+    author_email='brwnjm@gmail.com',
+    license='MIT',
+    keywords = "bioinformatics",
+    url='https://github.com/brwnjm/umitools',
+    entry_points={
+        'console_scripts': [
+            'umitools = umitools.umitools:main',
+        ]
+    },
+    install_requires=['pysam', 'toolshed'],
+    zip_safe=False,
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Utilities'
+    ],
+)
---
+++ umitools/__init__.py
@@ -0,0 +1,1 @@
+from ._version import __version_info__, __version__
